cae031c2c846bec7b366e14132a1344241a13071
devasher <hlxstc680@gmail.com>
2025-07-31 18:23:35 +0800

    init grammy tgbot

---
+++ tgbot-ts/src/index.ts
@@ -0,0 +1,18 @@
+import { Bot, Context, webhookCallback } from 'grammy';
+
+export interface Env {
+	BOT_INFO: string;
+	BOT_TOKEN: string;
+}
+
+export default {
+	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		const bot = new Bot(env.BOT_TOKEN, { botInfo: JSON.parse(env.BOT_INFO) });
+
+		bot.command('start', async (ctx: Context) => {
+			await ctx.reply('Hello, world!');
+		});
+
+		return webhookCallback(bot, 'cloudflare-mod')(request);
+	},
+} satisfies ExportedHandler<Env>;
